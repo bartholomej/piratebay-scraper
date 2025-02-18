6379ac19979c10d461c3a47196966172c767fd42
BART! <bart@bartweb.cz>
2025-02-18 21:38:35 +0100

    chore(deps): update vitest + tsx

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node'
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/**',
+        'dist/**',
+        '**/*.test.ts',
+        '**/*.config.ts'
+      ]
+    }
   }
 });
